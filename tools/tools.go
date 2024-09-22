0fa757ab44cb2965ff8cbe3ce7c24282de6a2383
winebarrel <sugawara@winebarrel.jp>
2024-09-22 15:04:15 +0900

    Add docs

---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+//go:build generate
+
+package tools
+
+import (
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
+
+//go:generate terraform fmt -recursive ../examples/
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name oneshot
